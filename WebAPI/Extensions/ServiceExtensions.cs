d6ae949651cdbcdfa9259ad413a71f67afa9a561
Enes Yıldırım <enes.yildirim@std.yeditepe.edu.tr>
2026-01-13 02:00:21 +0300

    add postgre migraions

--- WebAPI/Extensions/ServiceExtensions.cs
+++ WebAPI/Extensions/ServiceExtensions.cs
@@ -14,8 +14,8 @@ namespace WebAPI.Extensions
     public static class ServiceExtensions
     {
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
-            services.AddDbContext<RepositoryContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+        services.AddDbContext<RepositoryContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("sqlConnection")));
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
